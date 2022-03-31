56ca165203e3f917a34ed73a37f062802909c48f
TitouanCharrier <70266642+TitouanCharrier@users.noreply.github.com>
2022-03-31 17:34:04 +0200

    Add files via upload

---
+++ Front/front.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Cy choix</title>
+    <link rel="stylesheet" href="front.css">
+  </head>
+  <body>
+    <h1>Bienvenue</h1>
+
+
+
+  </body>
+</html>
